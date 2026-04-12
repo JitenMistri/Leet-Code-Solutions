00703af3a5714a3d4ec66363db301a4052dd69f9
Jiten Mistri <44949991+JitenMistri@users.noreply.github.com>
2026-04-12 23:31:31 +0530

    234. Palindrome Linked List

---
+++ Solutions/LeetCode-234.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode slow = head, fast = head;
+        while(fast !=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalfHead = reverse(slow);
+        ListNode secondHalfCopy = secondHalfHead;
+
+        boolean result = true;
+        ListNode p1 = head, p2 = secondHalfHead;
+        while(p2 != null){
+            if(p1.val != p2.val){
+                result = false;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        reverse(secondHalfCopy);
+        return result;
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode prev = null, curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
